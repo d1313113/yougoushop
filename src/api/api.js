5010a5806d5ecf9491bb4f7f539c04d2e83c8436
dcc <cumelmell@foxmail.com>
2018-05-15 16:14:27 +0800

    改成API获取数据

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
  * @Author: Cumelmell
  * @Date: 2018-05-15 11:27:33
  * @Last Modified by: Cumelmell
- * @Last Modified time: 2018-05-15 15:08:28
+ * @Last Modified time: 2018-05-15 16:11:11
  */
 // 1.0.1引入封装的weRequse方法
 import weRequset from '../utils/wxRequest'
@@ -13,13 +13,18 @@ import appConfig from '../utils/appConfig'
 weRequset.baseUrl = `${appConfig.HTTP_BAST_URL}/api/public/v1/`
 
 // 1.0.4封装请求API,类似axios
+// 获取轮播图图片
 const getHomeSwiper = params => weRequset('home/swiperdata', params).then(res => res.data)
 
-// 1.0.4封装请求API,类似axios
+// 获取分类标签
 const getNav = params => weRequset('home/catitems', params).then(res => res.data)
+
+// 获取楼层数据
+const getFool = params => weRequset('home/floordata', params).then(res => res.data)
 
 // 1.0.5把封装的API导出,这里是统一导出
 export default {
   getHomeSwiper,
-  getNav
+  getNav,
+  getFool
 }
