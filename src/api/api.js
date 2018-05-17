7269ac72104fb5d521445b014fba9ef3edef9739
dcc <cumelmell@foxmail.com>
2018-05-17 15:17:17 +0800

    初始化商品详情页

--- src/api/api.js
+++ src/api/api.js
@@ -2,29 +2,33 @@
  * @Author: Cumelmell
  * @Date: 2018-05-15 11:27:33
  * @Last Modified by: Cumelmell
- * @Last Modified time: 2018-05-15 16:11:11
+ * @Last Modified time: 2018-05-17 15:10:15
  */
 // 1.0.1引入封装的weRequse方法
-import weRequset from '../utils/wxRequest'
+import wxRequset from '../utils/wxRequest'
 // 1.0.2引入全局路径
 import appConfig from '../utils/appConfig'
 
 // 1.0.3设置请求API的根路径
-weRequset.baseUrl = `${appConfig.HTTP_BAST_URL}/api/public/v1/`
+wxRequset.baseUrl = `${appConfig.HTTP_BAST_URL}/api/public/v1/`
 
 // 1.0.4封装请求API,类似axios
 // 获取轮播图图片
-const getHomeSwiper = params => weRequset('home/swiperdata', params).then(res => res.data)
+const getHomeSwiper = params => wxRequset('home/swiperdata', params).then(res => res.data)
 
 // 获取分类标签
-const getNav = params => weRequset('home/catitems', params).then(res => res.data)
+const getNav = params => wxRequset('home/catitems', params).then(res => res.data)
 
 // 获取楼层数据
-const getFool = params => weRequset('home/floordata', params).then(res => res.data)
+const getFool = params => wxRequset('home/floordata', params).then(res => res.data)
+
+// 获商品详情
+const getGoodsDetail = params => wxRequset('goods/detail', params).then(res => res.data)
 
 // 1.0.5把封装的API导出,这里是统一导出
 export default {
   getHomeSwiper,
   getNav,
-  getFool
+  getFool,
+  getGoodsDetail
 }
